e2c445f9f02260d911e839b0f725e30bcd97dfa1
Patrik <datascience@dilena.ch>
2020-06-18 21:15:51 +0200

    update

--- udemy/udemy_dataframe.R
+++ udemy/udemy_dataframe.R
@@ -87,3 +87,28 @@ stats[stats$Income.Group == 'High income', ] # wir können natürlich auch kateg
 # Nachfolgend noch eine kleine Überprüfung der Korrelation zwischen Geburtsrate und Internetbenutzung
 cor(stats$Internet.users,stats$Birth.rate)
 scatter.smooth(stats$Internet.users,stats$Birth.rate)
+
+# ------ Kurzeinführung in qplot (ggplot)
+library(ggplot2)
+qplot(data=stats, x=Internet.users) # Erstellt ein Distplot
+qplot(data=stats, x=Income.Group, y=Birth.rate) # Erstellt ein Scatterplot
+qplot(data=stats, x=Income.Group, y=Birth.rate, size=I(3)) # Erstellt ebenfalls ein Scatterplot mit grösseren Punkten
+qplot(data=stats, x=Income.Group, y=Birth.rate, size=I(2),color=I('blue')) # Wir fügen nun noch eine Farbe dazu
+qplot(data=stats, x=Income.Group, y=Birth.rate, geom='boxplot') # Nun wollen wir eine bestimmten Plot erstellen
+
+# ----- Data Science Visualisierung 
+qplot(data=stats, x=Internet.users, y=Birth.rate) # Vergleich Anzahl Internet.users mit Geburtsrate
+qplot(data=stats, x=Internet.users, y=Birth.rate, size=I(2), color=('red'))
+# Nachfolgend werden wir den Vergleich erneut tätigen. Dabei wird die Grösse der Punkte auf die Geburtsrate
+# und die Farbe der Punkte nach Einkommensgruppe gestaltet.
+qplot(data=stats, x=Internet.users, y=Birth.rate, size=Birth.rate, color=Income.Group)
+
+# ----- Erstellen von Data Frames
+mydf <- data.frame(Countries_2012_Dataset,Codes_2012_Dataset,Regions_2012_Dataset) # Wir überführen nun diese 3 Vectors in ein Data Frame 
+head(mydf)
+colnames(mydf) <- c('Country','Code','Region') # Nun benennen wir noch die Spaltenname um
+
+# Wir können das wie in R gewohnt natürlich wieder etwas einfacher haben und benennen die Spaltenname 
+# direkt bei der Erstellung des DF 
+mydf <- data.frame(Country=Countries_2012_Dataset, Code=Codes_2012_Dataset, Region=Regions_2012_Dataset)
+head(mydf)
