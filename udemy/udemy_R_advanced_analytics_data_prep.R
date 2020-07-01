8f365e9f8f0562623d62627d0965fc2a06fc5331
Patrik <datascience@dilena.ch>
2020-07-01 07:46:03 +0200

    update

---
+++ udemy/udemy_R_advanced_analytics_data_prep.R
@@ -0,0 +1,29 @@
+fin <- read.csv('P3-Future-500-The-Dataset.csv')
+str(fin) 
+summary(fin)
+
+'''
+Mittels der Structure Befehls können wir ja sehen, ob R aus dem importierten Datensatz
+bereits Factoren erstellt hat. Hier aber nochmal einen kurzen Recap was Factors sind.
+
+Ein Factor ist grundsätzlich ein Anzahl kategorischer Werte eines Features, welche R erkennt
+und für sich nummeriert.
+
+Mittels str sehen wir auch, welche Datentypen R auch noch erkannt hat, bzw. umgewandelt hat.
+Während dieser Phase der Datenaufbereitung müssen wir uns immer überlegen, ob der zugewiesene Datentyp 
+überhaupt Sinn macht. Bspw. sehen wir, dass R die ID und Inception als Integer definiert. 
+Dieser Datentypen für diese Merkmale machen aber nicht unbedingt Sinn. 
+
+Auf der anderen Seite hat R bspw. die Werte des Merkmals Revenue (Einnahmen) und Expenses (Ausgaben)
+als Factor bzw. kategorische Werte erkannt was wiederum keinen Sinn macht, da wir ja mit diesen
+Werten arithmetische Operationen durchführen wollen.
+
+Wir haben hier also schon mal Punkte die wir bereinigen müssen.
+'''
+# Konvertierung von Datentypen
+
+# Die Konvertierung ist in R ganz einfach
+fin$ID <- factor(fin$ID) # Wandelt einen Int in eine Factor um
+fin$Inception <- factor(fin$Inception)
+str(fin) # Checken wir es kurz ... und es ist nun ein Factor
+
