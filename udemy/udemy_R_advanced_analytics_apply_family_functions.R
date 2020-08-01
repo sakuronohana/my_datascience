2a500cf0255aeb02b718bf7915df40d975b81f99
Patrik <datascience@dilena.ch>
2020-08-01 13:41:01 +0200

    Update udemy_R_advanced_analytics_apply_family_functions.R

--- udemy/udemy_R_advanced_analytics_apply_family_functions.R
+++ udemy/udemy_R_advanced_analytics_apply_family_functions.R
@@ -1,6 +1,6 @@
 '''
 Wir kommen nun zur Kür. Wir lernen nun R Funktionen, welche mit minimalen Code maximale Wirkung erzielen.
-Zudem werden wir folgenden Aufgabenstellung lösen:
+Zu diese Zweck werden wir folgenden Aufgabenstellung lösen:
 
 1.A table showing the annual averages of each observed metric for every city
 2.A table showing by how much temperature fluctuates each month from min to max (in %). Take min temperature as the base
@@ -18,3 +18,24 @@ Chicago <- read.csv('Chicago-C.csv', row.names =1) # Mir row.names definieren wi
 NewYork <- read.csv('NewYork-C.csv', row.names =1) 
 Houston <- read.csv('Houston-C.csv', row.names =1) 
 SanFrancisco <- read.csv('SanFrancisco-C.csv', row.names =1) 
+
+is.data.frame(Chicago) # Wir prüfen noch kurz ob es sich bei diesen Daten um ein DF handelt
+# Wir konvertieren nun die Datensätze in Matrixen
+
+Chicago <- as.matrix(Chicago)
+NewYork <- as.matrix(NewYork)
+Houston <- as.matrix(Houston)
+SanFrancisco <- as.matrix(SanFrancisco)
+
+# Im nächsten Schritt werden wir alle Datensätze in einer Liste zusammenfassen:
+wetter <- list(Chicago=Chicago, NewYork=NewYork, Houston=Houston, SanFrancisco=SanFrancisco)
+wetter$Chicago[1,]
+
+# Was ist jetzt eine Apply Function. Fangen wir doch einfach einmal mit einen Beispiel an
+# Im folgenden Beispiel nehmen wir den Datensatz 'Chicago' und berechnen den arithm. Mittelwert pro Zeile
+apply(Chicago,1,mean)
+apply(Chicago,1,max) # Hier noch ein Beispiel mit max
+
+# Wollen wir dieses Beispiel mit Spalten machen dann..
+apply(Chicago,2,mean)
+
